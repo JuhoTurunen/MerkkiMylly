e08858d0ad1fa7af5757f094a7e095c03a7fae7f
Juho Turunen <juho.turunen89@gmail.com>
2024-10-23 02:20:10 +0300

    Fixed import structure

--- app/routes.py
+++ app/routes.py
@@ -11,22 +11,8 @@ from flask import (
     request,
     session,
 )
-from .user_actions.gameplay import (
-    buy_upgrade,
-    get_user_score,
-    get_user_game_data,
-    list_upgrades,
-    get_leaderboard,
-)
 
-from .user_actions.account import (
-    create_user,
-    update_user_data,
-    check_password,
-    get_profiles,
-    update_session,
-    get_session_end,
-)
+from .user_actions import *
 
 main = Blueprint("main", __name__)
 
---
+++ app/user_actions/__init__.py
@@ -0,0 +1,16 @@
+from .gameplay import (
+    buy_upgrade,
+    get_user_score,
+    get_user_game_data,
+    list_upgrades,
+    get_leaderboard,
+)
+
+from .account import (
+    create_user,
+    update_user_data,
+    check_password,
+    get_profiles,
+    update_session,
+    get_session_end,
+)
